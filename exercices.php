705f8c3aa01f3ca1c324c7d74e6924d564f7d2ee
Julie J <jeannet.julie@gmail.com>
2022-08-23 15:05:43 +0200

    array_chunk et array_reduce

--- exercices.php
+++ exercices.php
@@ -18,11 +18,13 @@
         return $arg >= 18 ? true : false;
     }
     var_dump(estIlMajeure(25));
+
     function plusGrand($arg1, $arg2)
     {
         return max($arg1, $arg2);
     }
     var_dump(plusGrand(3, 7));
+
     function plusPetit($arg1, $arg2, $arg3)
     {
         return min($arg1, $arg2, $arg3);
@@ -35,12 +37,18 @@
     }
     var_dump(premierElementTableau([28, 36, 7]));
 
+    // on a asussi une fonction spécifique qui retourne la première clée d'un tableau associatif
+    var_dump(array_key_first([
+        "luc" => 32,
+        "sophie" => 36,
+        "julie" => 47
+    ]));
+
 
     function verificationPassword($arg)
     {
         return strlen($arg) >= 7 ? true : false;
     }
-
     var_dump(verificationPassword("1233d2358x"));
 
     function verificationPasswordOptimised($pass)
@@ -54,7 +62,6 @@
 
     function capital($arg)
     {
-
         switch ($arg) {
             case "France":
                 return "Paris";
@@ -81,21 +88,18 @@
                 return "Inconnu";
         };
     }
-
     var_dump(capital("France"));
 
     function listHTML($titre, $array)
     {
         if (is_null($titre) || empty($titre)) return null;
         if (empty($array)) return null;
-
         $result = "";
         foreach ($array as $element) {
             $result .= "<li>" . $element . "</li>";
         }
         return "<h3>" . $titre . "</h3><ul>" . $result . "</ul>";
     }
-
     var_dump(listHTML("Capitale", ["Paris", "Londres"]));
 
 
@@ -110,6 +114,39 @@
     // print_r($array);
     // return $array;
     // }
+
+    function creationPaires($array)
+    {
+        return array_chunk($array, 2);
+    }
+    var_dump(creationPaires([1, 2, 3, 4, 5, 6, 7]));
+
+
+    // l'utilisation de reduce en PHP que je crois n'avoir JAMAIS utlisée
+    function sum($out, $in)
+    {
+        $out += $in;
+        return $out;
+    }
+    function product($out, $in)
+    {
+        $out *= $in;
+        return $out;
+    }
+    function totalItems($array)
+    {
+        //   ok, mais je ne comprends pas la syntaxe "sum" pour référence à la fonction
+        return array_reduce($array, "sum");
+    }
+    var_dump(totalItems([1, 2, 3, 4, 5, 6]));
+
+    function productItems($array)
+    {
+        //   ok, mais je ne comprends pas la syntaxe "product" pour référence à la fonction
+        return array_reduce($array, "product");
+    }
+    var_dump(productItems([1, 2, 3, 4, 5, 6]))
+
 
     ?>
 
